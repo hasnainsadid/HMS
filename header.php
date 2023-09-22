40150c41b6064a3a3a521c9ec688b90e411278f7
Hasnain Sadid <hasnainsadid@gmail.com>
2023-09-22 22:11:46 +0600

    commit by sadid

---
+++ header.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>TrueMedicare Haven</title>
+  <link rel="shortcut icon" href="./admin/dist/img/fevicon.png" type="image/x-icon">
+  <link rel="stylesheet" href="./assets/css/maicons.css">
+  <link rel="stylesheet" href="./assets/css/bootstrap.css">
+  <link rel="stylesheet" href="./assets/vendor/owl-carousel/css/owl.carousel.css">
+  <link rel="stylesheet" href="./assets/vendor/animate/animate.css">
+  <link rel="stylesheet" href="./assets/css/theme.css">
+</head>
+    <body>
+    <div class="back-to-top"></div>
+
+    <header>
+      <nav class="navbar navbar-expand-lg navbar-light shadow-sm">
+        <div class="container">
+          <a class="navbar-brand" href="index.php"><span class="text-primary">TrueMedicare Haven</a>
+
+          <form action="#">
+            <div class="input-group input-navbar">
+              <div class="input-group-prepend">
+                <span class="input-group-text" id="icon-addon1"><span class="mai-search"></span></span>
+              </div>
+              <input type="text" class="form-control" placeholder="Enter keyword.." aria-label="Username" aria-describedby="icon-addon1">
+            </div>
+          </form>
+
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupport" aria-controls="navbarSupport" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarSupport">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="index.php">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="doctors.php">Doctors</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="about.php">About Us</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="contact.php">Contact</a>
+              </li>
+              <li class="nav-item">
+                <a class="btn btn-primary ml-lg-3" href="#">Login / Register</a>
+              </li>
+            </ul>
+          </div> <!-- .navbar-collapse -->
+        </div> <!-- .container -->
+      </nav>
+    </header>
